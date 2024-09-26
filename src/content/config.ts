e294edb6c56cb7c754dcd2fed6c90160d7ea8bdd
Charles Wang <czw@seas.upenn.edu>
2024-09-26 04:04:51 -0400

    renaming, fix videogif, 4.2.0

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,11 +3,11 @@ import { defineCollection, z } from "astro:content";
 const changelog = defineCollection({
   type: "content",
   schema: z.object({
-    version: z.string().regex(/^v[0-9]+\.[0-9]+\.[0-9]+$/g, {
+    version: z.string().regex(/^[0-9]+\.[0-9]+\.[0-9]+$/g, {
       message: "Version numbers should follow X.Y.Z format!",
     }),
     date: z.date(),
-    commit: z.string().length(40).optional(),
+    commit: z.string().length(40),
   }),
 });
 
